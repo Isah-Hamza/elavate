4b0d52c7ac08e5d77b81ac7a174dfa60bc4dcf70
Isah Hamza <itshamzy@gmail.com>
2025-01-05 18:16:53 +0100

    -

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/images/coach_logo.webp';
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { CgClose } from 'react-icons/cg';
 
 const Header = () => {
+    const [ open, setOpen] = useState(false);
+    const toggle = () => setOpen(!open);
+
   return (
     <div className="w-full ">
-        <div className='max-w-[1400px] bg-white rounded-lg mx-auto flex items-center justify-between gap-10 px-10 py-3'>
+        <div className={`max-w-[96%] sm:max-w-[1400px] bg-white rounded-lg mx-auto flex ${open ? 'flex-col md:flex-row': ''} md:flex-row items-center justify-between gap-10 px-10 py-3`}>
             <img className='max-w-40' src={logo} alt="logo" />
-            <ul className='hidden max-w-[800px] flex-1 font-bold md:flex justify-between text-lg gap-5' >
+            <ul className={`text-center md:text-left max-w-[800px] flex-1 font-bold ${ open ? 'grid' : 'hidden'} md:flex justify-between text-lg gap-7 md:gap-5`} >
                 <li>Home</li>
                 <li>About Us</li>
                 <li>What We Do</li>
                 <li>Contact</li>
             </ul>
-            <span className='block md:hidden' > <HiOutlineMenuAlt4 size={30} /> </span>
+            { 
+            !open ? <span onClick={toggle} className='block md:hidden' > <HiOutlineMenuAlt4 size={30} /> </span> :
+            <span onClick={toggle} className='block md:hidden' > <CgClose size={30} /> </span>}
         </div>
     </div>
   )
